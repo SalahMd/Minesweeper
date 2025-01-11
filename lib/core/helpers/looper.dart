371239e9ef3b6e39f2aa41d1cad95abaea3c882d
SalahMd <salahaldeenmdaghmesh@gmail.com>
2025-01-11 12:56:33 +0300

    using looper methode

--- lib/core/helpers/looper.dart
+++ lib/core/helpers/looper.dart
@@ -1,8 +1,8 @@
  
  
- looper(int iVal, int jVal, Function(int i, int j) callBack) {
-    for (int i = 0; i < iVal; i++) {
-      for (int j = 0; j < jVal; j++) {
+ looper(int iVal, int jVal, Function(int i, int j) callBack,{int startIValue=0,int startJValue=0}) {
+    for (int i = startIValue; i < iVal; i++) {
+      for (int j = startJValue; j < jVal; j++) {
         callBack(i, j);
       }
     }
--- lib/features/home/data/cells.dart
+++ lib/features/home/data/cells.dart
@@ -4,13 +4,11 @@ import 'package:untitled/features/home/data/board.dart';
 
 class Cells {
   final int numOfRows, numOfColumns;
-
   Cells({required this.numOfRows, required this.numOfColumns});
 
   void closeCells(Board board) {
     looper(numOfRows, numOfColumns, (j, i) {
-      if (board.openedCells[j][i].last == board.backwardMoves.length &&
-          board.openedCells[j][i].first) {
+      if(notEmptyMoves(board,j,i)) {
         board.cells[j][i] = null;
         board.openedCells[j][i].first = false;
         board.openedCells[j][i].last = 0;
@@ -20,34 +18,28 @@ class Cells {
   }
 
   void openCells(int x, int y, Board board, BuildContext ctx) {
-    for (int i = x - 1; i <= x + 1; i++) {
-      for (int j = y - 1; j <= y + 1; j++) {
-        if (valid(i, j) &&
-            !board.openedCells[i][j].first &&
-            !board.mines[i][j]) {
-          board.openedCells[i][j] = [true, board.backwardMoves.length];
-          int count = countMines(i, j, board);
-          if (count == 0) {
-            openCells(i, j, board, ctx);
-          } else {
-            board.cells[i][j] = count;
-          }
-          board.numOfOpenedCells++;
+    looper(x + 2, y + 2, (i, j) {
+      if (valid(i, j) && !board.openedCells[i][j].first && !board.mines[i][j]) {
+        board.openedCells[i][j] = [true, board.backwardMoves.length];
+        int count = countMines(i, j, board);
+        if (count == 0) {
+          openCells(i, j, board, ctx);
+        } else {
+          board.cells[i][j] = count;
         }
+        board.numOfOpenedCells++;
       }
-    }
+    }, startIValue: x - 1, startJValue: y - 1);
     board.checkWin(board, ctx);
   }
 
   int countMines(int x, int y, Board board) {
     int ctn = 0;
-    for (int i = x - 1; i <= x + 1; i++) {
-      for (int j = y - 1; j <= y + 1; j++) {
-        if (valid(i, j) && board.mines[i][j]) {
-          ctn++;
-        }
+    looper(x + 2, y + 2, (i, j) {
+      if (valid(i, j) && board.mines[i][j]) {
+        ctn++;
       }
-    }
+    }, startIValue: x - 1, startJValue: y - 1);
     return ctn;
   }
 
@@ -66,5 +58,10 @@ class Cells {
     return !board.openedCells[posX][posY].first &&
         !board.isLost &&
         !board.isWin;
+  }
+
+  notEmptyMoves(Board board, int j, int i) {
+    return board.openedCells[j][i].last == board.backwardMoves.length &&
+        board.openedCells[j][i].first;
   }
 }
